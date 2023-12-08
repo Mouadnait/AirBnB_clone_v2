f13d4a7e42d9d903afe90a800ec78a7533f1d7b0
mouadnait <moadnait6@gmail.com>
2023-12-08 02:02:33 +0100

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,30 +9,7 @@ from datetime import datetime
 
 
 env.hosts = ['54.172.114.96', '54.160.113.130']
-
-
-def do_pack():
-    """ generates a .tgz archive from the contents of the web_static
-
-    All files in the folder web_static must be added to the final archive.
-    All archives must be stored in the folder versions.
-    The name of the archive created must be:
-        web_static_<year><month><day><hour><minute><second>.tgz
-    The function do_pack must return the archive path if the archive has
-    been correctly generated. Otherwise, it should return None.
-
-    Returns:
-        fabric.operations._AttributeString: archive path.
-    """
-    timestr = datetime.now().strftime("%Y%m%d%H%M%S")
-    try:
-        # create folder versions if it doesn’t exist
-        local("mkdir -p versions")
-        # extract the contents of a tar archive
-        local(f"tar -cvzf versions/web_static_{timestr}.tgz web_static/")
-        return ("versions/web_static_{}.tgz".format(timestr))
-    except BaseException:
-        return None
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
@@ -44,7 +21,7 @@ def do_deploy(archive_path):
     Returns:
         Boolean: whether the archive is distributed or not
     """
-    if (os.path.isfile(archive_path) is False):
+    if not os.path.exists(archive_path):
         return False
 
     try:
@@ -56,22 +33,22 @@ def do_deploy(archive_path):
         # upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
         # Create new directory for release
-        run("sudo mkdir -p {}".format(new_folder))
+        run("mkdir -p {}".format(new_folder))
         # Untar archive
-        run("sudo tar -xzf /tmp/{} -C {}".
+        run("tar -xzf /tmp/{} -C {}".
             format(new_comp, new_folder))
         # Delete the archive from the web server
-        run("sudo rm /tmp/{}".format(new_comp))
+        run("rm /tmp/{}".format(new_comp))
         # Move extraction to proper directory
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("mv {}/web_static/* {}/".format(new_folder, new_folder))
         # Delete first copy of extraction after move
-        run("sudo rm -rf {}/web_static".format(new_folder))
+        run("rm -rf {}/web_static".format(new_folder))
         # Delete the symbolic link /data/web_static/current from the web server
-        run('sudo rm -rf /data/web_static/current')
+        run('rm -rf /data/web_static/current')
         # Create new the symbolic link /data/web_static/current on web server,
         # linked to the new version of your code,
         # (/data/web_static/releases/<archive filename without extension>
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        run("ln -s {} /data/web_static/current".format(new_folder))
 
         print('New version deployed!')
         return True
