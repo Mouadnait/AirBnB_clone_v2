093582328cbd1b5640fddf3eb95b436c73673468
mouadnait <moadnait6@gmail.com>
2023-12-08 03:45:03 +0100

    2. Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,10 +2,9 @@
 """
 script that distributes archive to webservers
 """
-import os.path
-from fabric.api import *
-from fabric.operations import run, put, sudo
+from fabric.api import put, run, env, local
 from datetime import datetime
+from os.path import exists
 
 
 env.hosts = ['54.172.114.96', '54.160.113.130']
@@ -47,47 +46,33 @@ def do_deploy(archive_path):
     Returns:
         Boolean: whether the archive is distributed or not
     """
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
-    # Uncompress the archive to the folder,
-    # /data/web_static/releases/<archive filename without extension>
-    # on the web server
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
-
     try:
+        # Uncompress the archive to the folder,
+        # /data/web_static/releases/<archive filename without extension>
+        # on the web server
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         # upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/{}".format(file_name))
-
+        put(archive_path, '/tmp/')
         # Create new directory for release
-        run("mkdir -p {}".format(folder_path))
-
+        run('mkdir -p {}{}/'.format(path, no_ext))
         # Untar archive
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
         # Delete the archive from the web server
-        run("rm -rf /tmp/{}".format(file_name))
-
+        run('rm /tmp/{}'.format(file_n))
         # Move extraction to proper directory
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         # Delete first copy of extraction after move
-        run("rm -rf {}web_static".format(folder_path))
-
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         # Delete the symbolic link /data/web_static/current from the web server
-        run("rm -rf /data/web_static/current")
-
+        run('rm -rf /data/web_static/current')
         # Create new the symbolic link /data/web_static/current on web server,
         # linked to the new version of your code,
         # (/data/web_static/releases/<archive filename without extension>
-        run("ln -s {} /data/web_static/current".format(folder_path))
-
-        print('New version deployed!')
-        success = True
-
-    except Exception:
-        success = False
-        print("Could not deploy")
-    return success
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
